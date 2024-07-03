108ecea73294f5406c61ad12534c03d0ff355565
YassineNabat <yassarj@DESKTOP-QF29U4J>
2024-07-03 23:37:22 +0100

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #!/usr/bin/pup
+# Ensure pip3 is installed (if not already managed)
+package { 'python3-pip':
+	ensure => installed,
+}
 # Install an especific version of flask (2.1.0)
 package { 'flask':
 	ensure   => '2.1.0',
 	provider => 'pip3',
+	require  => Package['python3-pip'],
 }
