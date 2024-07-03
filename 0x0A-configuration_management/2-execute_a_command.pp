fe7f42c65d6c590ab7b3947da3eed374570f4146
YassineNabat <yassarj@DESKTOP-QF29U4J>
2024-07-03 23:39:47 +0100

    exe commande

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # execute pkill command to kill bashscript file killmenow
-exec { 'pkill':
-	command => 'pkill -9 -f killmenow',
-	path    => ['/usr/bin', '/usr/sbin', '/bin']
-}
+
+exec { 'pkill killmenow' :
+    path    => '/bin/',
+    command => 'pkill killmenow',
+    }
